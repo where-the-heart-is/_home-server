34dc827f4f50027ee09254953ce3479e1a360ca2
Will Carpenter <willcarpenteriii@gmail.com>
2017-06-27 14:45:21 -0600

    local

--- db/queries.js
+++ db/queries.js
@@ -1,56 +1,47 @@
-const knex = require('./knex');
+const knex = require( './knex' );
 
 module.exports = {
-  getUserByID: function(id) {
-    return knex.select('email', 'first_name', 'last_name').from('account').where('account.id', id).first();
-  },
-  getUserProperties(id) {
-    return knex('account').where('account.id', id).select('is_landlord').first()
-      .then(account => {
-        if (account.is_landlord) {
-          return knex.select('*').from('tenant_property')
-            .join('property', 'property_id', 'property.id').where('landlord_id', id)
-            .join('account', 'tenant_id', 'account.id')
-            .join('location', 'location_id', 'location.id')
-
-        } else {
-          return knex.select('*').from('tenant_property')
-            .join('property', 'property_id', 'property.id').where('tenant_id', id)
-            .join('account', 'tenant_id', 'account.id')
-            .join('location', 'location_id', 'location.id')
-        }
-      });
-  },
-  getAllPropertyDetails(id) {
-      let promises = [];
-    
-      function parsePropDocs(docs) {
-          
-          const documents = {
-              documents: docs[0][0],
-              maintenance: docs[1][0]
-          }
-          
-          return documents;
-          
-      }
-      
-      return knex('account').where('account.id', id).select('is_landlord').first()
-        .then(account => {
-            if (account.is_landlord) {
-              return knex.select('*').from('tenant_property')
-                .join('property', 'property_id', 'property.id').where('landlord_id', id)
-                .join('account', 'tenant_id', 'account.id')
-                .join('location', 'location_id', 'location.id')
-                    .then(() => {
-                        promises.push(knex('documents').where('property_id', id))
-                        promises.push(knex('maintenance').where('property_id', id))
-                        
-                        return Promise.all(promises).then(parsePropDocs);
-                    
-                    
-                    })
-            }
-      });
-  }
+	getUserByID: function ( id ) {
+		return knex.select( 'email', 'first_name', 'last_name' ).from( 'account' ).where( 'account.id', id ).first();
+	},
+	getUserProperties( id ) {
+		return knex( 'account' ).where( 'account.id', id ).select( 'is_landlord' ).first()
+			.then( account => {
+				if( account.is_landlord ) {
+					return knex.select( '*' ).from( 'tenant_property' )
+						.join( 'property', 'property_id', 'property.id' ).where( 'landlord_id', id )
+						.join( 'account', 'tenant_id', 'account.id' )
+						.join( 'location', 'location_id', 'location.id' )
+				} else {
+					return knex.select( '*' ).from( 'tenant_property' )
+						.join( 'property', 'property_id', 'property.id' ).where( 'tenant_id', id )
+						.join( 'account', 'tenant_id', 'account.id' )
+						.join( 'location', 'location_id', 'location.id' )
+				}
+			} );
+	},
+	getAllPropertyDetails( id ) {
+		let promises = [];
+		function parsePropDocs( docs ) {
+			const documents = {
+				documents: docs[ 0 ][ 0 ],
+				maintenance: docs[ 1 ][ 0 ]
+			}
+			return documents;
+		}
+		return knex( 'account' ).where( 'account.id', id ).select( 'is_landlord' ).first()
+			.then( account => {
+				if( account.is_landlord ) {
+					return knex.select( '*' ).from( 'tenant_property' )
+						.join( 'property', 'property_id', 'property.id' ).where( 'landlord_id', id )
+						.join( 'account', 'tenant_id', 'account.id' )
+						.join( 'location', 'location_id', 'location.id' )
+						.then( () => {
+							promises.push( knex( 'documents' ).where( 'property_id', id ) )
+							promises.push( knex( 'maintenance' ).where( 'property_id', id ))
+							return Promise.all( promises ).then( parsePropDocs );
+						} )
+				}
+			} );
+	}
 }
