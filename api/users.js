d8ca9c89441b53fdc08d626d58128ca27aaf4e79
Will Carpenter <willcarpenteriii@gmail.com>
2017-06-28 10:54:29 -0600

    local change

--- api/users.js
+++ api/users.js
@@ -40,14 +40,10 @@ router.get('/:id', isValidId, (req, res, next) => {
           lastName: property.last_name,
           email: property.email
         });
-
-      });
-      res.json(collectionOfProperties)
-
+    });
+    res.json(collectionOfProperties)
+      
     });
 });
-
-
-
 
 module.exports = router;
